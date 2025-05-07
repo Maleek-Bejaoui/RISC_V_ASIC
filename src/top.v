c022d17a1d99b223fbf6b83bac0e8abb60d71976
Mathieu Escouteloup <147056108+mescoutelou@users.noreply.github.com>
2025-05-07 11:24:33 +0200

    Update top.v

--- src/top.v
+++ src/top.v
@@ -17,29 +17,31 @@ module tt_um_top (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
  //   assign uio_out = 0;
-    assign uo_out[7:1]  = 7'b1111111;
-    assign uio_oe[7:1]  = 7'b1000000;
+//    assign uo_out[7:1]  = 7'b1111111;
+//    assign uio_oe[7:1]  = 7'b1000000;
 
     
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{uio_in[7:2], 6'b000000};
+    
    // wire _unused1 = &{uio_in, 8'b0};
+    
+    // Default
+    assign uo_out[7:1]  = ui_in[7:2] + uio_in[7:2];
+    wire _unused = &{ena, 7'b000000};
 
  Sys m_riscV
     (.clock(clk),
      .reset(!rst_n),
-     .io_i_boot(ena),
-     .io_b_gpio_in(ui_in),
-     .io_b_uart_rx(uio_in[0]),
+     .io_i_boot(ui_in[1]),
+     .io_b_uart_rx(ui_in[0]),
+     .io_b_uart_tx(uo_out[0]),
      
-     .io_b_gpio_eno(uo_out),
+     .io_b_gpio_eno(uio_oe),
      .io_b_gpio_out(uio_out),
-     .io_b_uart_tx(uio_oe[0])
-     
-     
+     .io_b_gpio_in(uio_in),
     );
 
 
